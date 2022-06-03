8726e613645ef6b7e372357d385003966ea1c43d
Jakob Gillich <jakob@gillich.me>
2022-06-04 01:27:06 +0200

    fixes and visual indicator

---
+++ README.md
@@ -0,0 +1,19 @@
+# godot-snappy
+
+A vertex snapping plugin for Godot 4 (currently in alpha).
+
+## How to install
+
+Clone this directory into `addons/` and enable the addon in your project settings.
+
+## How to use
+
+1. Select a object in the 3D view and move cursor over it
+2. Press and hold `v` to view origin point
+3. Left mouse button drag to snap to any other object
+
+
+## Caveats
+
+* You must hover on a vertex, being close is not good enough. Help wanted!
+* Plugin is new and may have bugs.
--- snappy.gd
+++ snappy.gd
@@ -6,14 +6,18 @@ const ray_length = 1000
 var selection = get_editor_interface().get_selection()
 var selected : Node3D = null
 
+@onready var Marker = preload("res://addons/snappy/marker.tscn")
+
 var origin = Vector3()
+var origin_2d = null
+
+
 
 func _enter_tree():
 	selection.connect("selection_changed", _on_selection_changed)
-	print("_enter_tree")
 
-func _exit_tree():
-	print("_exit_tree")
+#func _exit_tree():
+	
 	
 func _handles(object):
 	if object is Node3D:
@@ -21,26 +25,29 @@ func _handles(object):
 	return false
 
 func _forward_3d_draw_over_viewport(overlay):
-	#overlay.draw_style_box(Rect2(overlay.get_local_mouse_position()), 16)
-	#overlay.draw_arc(overlay.get_local_mouse_position(), 16.0, deg2rad(0), deg2rad(360), 16, Color.YELLOW)
-#	print(overlay.get_local_mouse_position(), " ", Vector2(origin.x, origin.z))
-#	overlay.draw_circle(overlay.get_local_mouse_position(), 16, Color.YELLOW)
-#	overlay.draw_circle(Vector2(origin.x, origin.z), 4, Color.YELLOW)
+	if origin_2d != null:
+		overlay.draw_circle(origin_2d, 4, Color.YELLOW)
 	pass
 
 func _forward_3d_gui_input(camera, event):
-	if event is InputEventMouse and selected != null:
+	if event is InputEventMouse and selected != null and Input.is_key_pressed(KEY_V):
 		var from = camera.project_ray_origin(event.position)
 		var to = from + camera.project_ray_normal(event.position) * ray_length
 		var direction = camera.project_ray_normal(event.position)
-		
+
 		if event.button_mask != MOUSE_BUTTON_LEFT:
 			var meshes = find_meshes(selected)
 			origin = find_closest_point(meshes, from, direction)
-			#update_overlays()
-			return false
-		elif Input.is_key_pressed(KEY_V):
-			if origin != null:
+
+			if origin != Vector3.ZERO:
+				origin_2d = camera.unproject_position(origin)
+			else:
+				origin_2d = null
+			update_overlays()
+		else:
+			if origin != Vector3.ZERO:
+				origin_2d = camera.unproject_position(origin)
+				update_overlays()
 				var ids = RenderingServer.instances_cull_ray(from, to, selected.get_world_3d().scenario)
 				var meshes = []
 				for id in ids:
@@ -50,10 +57,13 @@ func _forward_3d_gui_input(camera, event):
  
 				var target = find_closest_point(meshes, from, direction)
 				if target != Vector3.ZERO:
-					selected.translate(target - origin)
+					selected.translate((target * selected.global_transform.basis) - (origin * selected.global_transform.basis))
 					origin = target
+					
 				return true
-				
+	if not Input.is_key_pressed(KEY_V):
+		origin_2d = null
+		update_overlays()
 	return false
 
 func _on_selection_changed():
@@ -74,19 +84,21 @@ func find_meshes(node : Node3D) -> Array:
 	return meshes
 	
 func find_closest_point(meshes : Array, from : Vector3, direction) -> Vector3:
-	var points = []
-	var closest = Vector3.ZERO
+	var closest := Vector3.ZERO
+	
 	for mesh in meshes:
 		var faces = mesh.get_mesh().get_faces()
 		for i in range(0, faces.size(), 3):
 			var p1: Vector3 = mesh.global_transform * faces[i]
 			var p2: Vector3 = mesh.global_transform * faces[i + 1]
 			var p3: Vector3 = mesh.global_transform * faces[i + 2]
-			if Geometry3D.ray_intersects_triangle(from, direction, p1, p2, p3) != null:
-				points += [p1, p2, p3]
-
-	for point in points:
-		if closest == Vector3.ZERO or from.distance_to(closest) > from.distance_to(point):
-			closest = point
-			
+			var intersection = Geometry3D.ray_intersects_triangle(from, direction, p1, p2, p3)
+			if intersection != null:
+				var vertex_point = null
+				for point in [p1, p2, p3]:
+					if vertex_point == null or intersection.distance_to(vertex_point) > intersection.distance_to(point):
+						vertex_point = point
+				if closest == Vector3.ZERO or from.distance_to(closest) > from.distance_to(vertex_point):
+					closest = vertex_point
+	
 	return closest
