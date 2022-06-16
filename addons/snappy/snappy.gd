4f87f2ddc116207cb7430542ce23af6d061088a9
realgdman <83962805+realgdman@users.noreply.github.com>
2022-06-16 14:52:33 +0300

    Port to godot 3.x

--- addons/snappy/snappy.gd
+++ addons/snappy/snappy.gd
@@ -1,33 +1,33 @@
-@tool
+tool
 extends EditorPlugin
 
 const RAY_LENGTH = 1000
 # Workaround for Vector3.INF GDScript issue https://github.com/godotengine/godot/issues/61643
 const VECTOR_INF = Vector3(INF, INF, INF)
 
-@onready var undo_redo = get_undo_redo()
-var undo_position = null
+onready var undo_redo = get_undo_redo()
+var undo_transform = null
 
 var selection = get_editor_interface().get_selection()
-var selected : Node3D = null
+var selected : Spatial = null
 var dragging = false
 var origin = Vector3()
 var origin_2d = null
 
 func _enter_tree():
-	selection.connect("selection_changed", _on_selection_changed)
+	selection.connect("selection_changed", self, "_on_selection_changed")
 	
-func _handles(object):
-	if object is Node3D:
+func handles(object):
+	if object is Spatial:
 		return true
 	return false
 
-func _forward_3d_draw_over_viewport(overlay):
+func forward_spatial_draw_over_viewport(overlay):
 	if origin_2d != null:
-		overlay.draw_circle(origin_2d, 4, Color.YELLOW)
+		overlay.draw_circle(origin_2d, 4, Color.yellow)
 	pass
 
-func _forward_3d_gui_input(camera, event):
+func forward_spatial_gui_input(camera, event):
 	# We need mouse events to get the cursor position
 	# This means pressing V when no mouse events are being sent does nothing
 	# It's rarely noticable though
@@ -35,14 +35,13 @@ func _forward_3d_gui_input(camera, event):
 		return false
 		
 	#if event is InputEventMouse:
-	var now_dragging = event.button_mask == MOUSE_BUTTON_LEFT and Input.is_key_pressed(KEY_V)
+	var now_dragging = event.button_mask == BUTTON_MASK_LEFT and Input.is_key_pressed(KEY_V)
 	if dragging and not now_dragging and origin != VECTOR_INF:
 		undo_redo.create_action("Snap vertex")
-		undo_redo.add_do_property(selected, "position", selected.position)
-		undo_redo.add_undo_property(selected, "position", undo_position)
+		undo_redo.add_do_property(selected, "transform", selected.transform)
+		undo_redo.add_undo_property(selected, "transform", undo_transform)
 		undo_redo.commit_action()
 	dragging = now_dragging
-
 		
 	if Input.is_key_pressed(KEY_V):
 		var from = camera.project_ray_origin(event.position)
@@ -52,7 +51,7 @@ func _forward_3d_gui_input(camera, event):
 		if not dragging:
 			var meshes = find_meshes(selected)
 			origin = find_closest_point(meshes, from, direction)
-			undo_position = selected.position
+			undo_transform = selected.transform
 
 			if origin != VECTOR_INF:
 				origin_2d = camera.unproject_position(origin)
@@ -62,16 +61,16 @@ func _forward_3d_gui_input(camera, event):
 		elif origin != VECTOR_INF:
 			origin_2d = camera.unproject_position(origin)
 			update_overlays()
-			var ids = RenderingServer.instances_cull_ray(from, to, selected.get_world_3d().scenario)
+			var ids = VisualServer.instances_cull_ray(from, to, selected.get_world().scenario)
 			var meshes = []
 			for id in ids:
 				var obj = instance_from_id(id)
-				if obj != selected and obj.get_parent() != selected and obj is Node3D:
+				if obj != selected and obj.get_parent() != selected and obj is Spatial:
 					meshes += find_meshes(obj)
 
 			var target = find_closest_point(meshes, from, direction)
 			if target != VECTOR_INF:
-				selected.translate((target * selected.global_transform.basis) - (origin * selected.global_transform.basis))
+				selected.translate((selected.global_transform.basis.xform(target)) - (selected.global_transform.basis.xform(origin)))
 				origin = target
 			return true
 	else:
@@ -82,18 +81,18 @@ func _forward_3d_gui_input(camera, event):
 
 func _on_selection_changed():
 	var nodes = selection.get_selected_nodes()
-	if nodes.size() > 0 and nodes[0] is Node3D:
+	if nodes.size() > 0 and nodes[0] is Spatial:
 		selected = nodes[0]
 	else:
 		selected = null
 		origin = null
 
-func find_meshes(node : Node3D) -> Array:
+func find_meshes(node : Spatial) -> Array:
 	var meshes : Array = []
-	if node is MeshInstance3D:
+	if node is MeshInstance:
 		meshes.append(node)
 	for child in node.get_children():
-		if child is Node3D:
+		if child is Spatial:
 			meshes += find_meshes(child)
 	return meshes
 	
@@ -114,7 +113,7 @@ func find_closest_point(meshes : Array, from : Vector3, direction : Vector3) -> 
 		var vertices = mesh.get_mesh().get_faces()
 		for i in range(vertices.size()):
 			var current_point: Vector3 = mesh.global_transform * vertices[i]
-			var current_on_ray := Geometry3D.get_closest_point_to_segment_uncapped(
+			var current_on_ray := Geometry.get_closest_point_to_segment_uncapped(
 				current_point, segment_start, segment_end)
 			var current_distance := current_on_ray.distance_to(current_point)
 			if closest == VECTOR_INF or current_distance < closest_distance:
